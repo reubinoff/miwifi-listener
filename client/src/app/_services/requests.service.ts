43b2508e18092f14ce584496efc0562b842c6c73
Moshe Reubinoff <moshe@ownbackup.com>
2022-07-17 14:23:26 +0300

    tests

--- client/src/app/_services/requests.service.ts
+++ client/src/app/_services/requests.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { WifiRequest } from '../_models/WifiRequest.model';
+
 
 const API_URL = 'https://func.listener.reubinoff.com/api/miwifi_scheduler';
 
